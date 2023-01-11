4e58fdeb25e9fcd3ceee284f705b73527dfcbaf1
endulum <apheum@zoho.com>
2023-01-11 12:25:32 -0800

    general looping update function, some tweaks

--- script.js
+++ script.js
@@ -26,6 +26,17 @@ function addBookToLibrary(book) {
 
     let read = document.createElement('small');
     read.textContent = book.read ? 'read' : 'not read yet' ;
+    read.addEventListener('click', function() {
+        if (book.read) {
+            read.textContent = 'not read yet';
+            book.read = false;
+            console.log(book.read);
+        } else {
+            read.textContent = 'read';
+            book.read = true;
+            console.log(book.read);
+        }
+    });
 
     library.appendChild(newBook);
 
@@ -40,22 +51,25 @@ function addBookToLibrary(book) {
     bottomHalf.appendChild(read);
 
     let remove = document.createElement('button');
-    // remove.classList.add('remove');
-    // let removeId = book.title.split(' '); removeId.push('-remove');
-    // // can't do this on one line so consolidated to above two lines.
-    // // this is to set up the removal button's id such that it is uniquely
-    // // identifiable with its parent book
-    // remove.setAttribute('id', removeId.join(''));
     remove.innerHTML = '<img src=\"resources/x-solid.svg\" alt=\"Remove\">';
     newBook.appendChild(remove);
-
     remove.addEventListener('click', function() {
         library.removeChild(newBook);
     });
 }
 
+// temp vars
+
 let hobbit = new Book('The Hobbit', 'J.R.R Tolkien', 314, false);
 let gatsby = new Book('The Great Gatsby', 'F. Scott Fitzgerald', 108, true);
 
-addBookToLibrary(hobbit);
-addBookToLibrary(gatsby);
+books.push(hobbit);
+books.push(gatsby);
+
+function updateLibrary() {
+    books.forEach(function (book) {
+        addBookToLibrary(book);
+    });
+}
+
+updateLibrary();
